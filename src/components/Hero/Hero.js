225e07014081f4f6db3d4b515e6c004bd31fc3ef
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-11-04 12:49:38 +0000

    Add Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={() => window.location = '#about'}>Learn More</Button>
       </LeftSection>
     </Section>
   </>
