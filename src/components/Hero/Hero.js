1ecdb63741bbbd8d5d37a2bb1b77c7d3f58e6319
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-12-19 13:09:41 +0000

    Add about myself in the hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        Hey Everyone, I'm Prajwal Jaiswal and i'm currntly pursuing my B.E. in Computer Engineering. I'm passionate about Full-Stack development and also interested in working on many Machine Learning projects.    
         </SectionText>
         <Button onClick={() => window.location = '#about'}>Learn More</Button>
       </LeftSection>
